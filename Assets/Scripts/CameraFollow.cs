62615d8379a66fe9715603a2a2e22a5b0f928ee2
fredlc <fredlefevre@outlook.com>
2021-01-30 17:40:53 -0500

    Implemented colliders on scene, character controller on hero and camera follow script

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class CameraFollow : MonoBehaviour
+{
+  [SerializeField] Transform target;
+  [SerializeField] float smoothing = 5.0f;
+
+  Vector3 offset;
+
+  private void Awake()
+  {
+    Assert.IsNotNull(target);
+  }
+
+  void Start()
+  {
+    offset = transform.position - target.position;
+  }
+
+
+  void Update()
+  {
+    Vector3 targetCameraPosition = target.position + offset;
+    transform.position = Vector3.Lerp(transform.position, targetCameraPosition, smoothing * Time.deltaTime);
+  }
+}
