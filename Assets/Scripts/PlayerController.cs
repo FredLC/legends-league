d502665f17aadb24764423f274f6e2f2a557dbbe
fredlc <fredlefevre@outlook.com>
2021-01-30 18:23:42 -0500

    Integrated hero animations and transitions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,16 +5,58 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
   [SerializeField] private float moveSpeed = 10.0f;
+  [SerializeField] private LayerMask layerMask;
+
   private CharacterController characterController;
+  private Vector3 currentLookTarget = Vector3.zero;
+  private Animator animator;
 
   private void Start()
   {
     characterController = GetComponent<CharacterController>();
+    animator = GetComponent<Animator>();
   }
 
   private void Update()
   {
     Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
     characterController.SimpleMove(moveDirection * moveSpeed);
+
+    if (moveDirection == Vector3.zero)
+    {
+      animator.SetBool("isWalking", false);
+    }
+    else
+    {
+      animator.SetBool("isWalking", true);
+    }
+
+    if (Input.GetMouseButtonDown(0))
+    {
+      animator.Play("DoubleChop");
+    }
+
+    if (Input.GetMouseButtonDown(1))
+    {
+      animator.Play("SpinAttack");
+    }
+  }
+
+  private void FixedUpdate()
+  {
+    RaycastHit hit;
+    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+    if (Physics.Raycast(ray, out hit, 500, layerMask, QueryTriggerInteraction.Ignore))
+    {
+      if (hit.point != currentLookTarget)
+      {
+        currentLookTarget = hit.point;
+      }
+
+      Vector3 targetPosition = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+      Quaternion rotation = Quaternion.LookRotation(targetPosition - transform.position);
+      transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * 10f);
+    }
   }
 }
