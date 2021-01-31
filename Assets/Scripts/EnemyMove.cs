8df41f8c88e02950dd41f68a683f868d6405dd19
fredlc <fredlefevre@outlook.com>
2021-01-31 18:50:57 -0500

    Implemented enemy navigation and animator controllers

---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Assertions;
+
+public class EnemyMove : MonoBehaviour
+{
+  [SerializeField] Transform player;
+  private NavMeshAgent navMeshAgent;
+  private Animator animator;
+
+  private void Awake()
+  {
+    Assert.IsNotNull(player);
+  }
+
+  void Start()
+  {
+    animator = GetComponent<Animator>();
+    navMeshAgent = GetComponent<NavMeshAgent>();
+  }
+
+  void Update()
+  {
+    navMeshAgent.SetDestination(player.position);
+  }
+}
